5c46521b2d464133edd281d30b21f1c19b0fc683
JefferyyYong <74395119+JefferyyYong@users.noreply.github.com>
2025-02-26 00:53:29 +0800

    Upload CSV file

--- src/routes/main.route.ts
+++ src/routes/main.route.ts
@@ -1,22 +1,9 @@
 import { FastifyInstance } from "fastify";
 import { filterReports, getReports, uploadReport } from "../functions";
 import { getUserData, updateUserPassword } from "../functions/user.function";
+import { uploadCSVAndSaveToFirestore } from "../functions/data-management.function";
 
 export async function mainRoute(fastify: FastifyInstance) {
-    fastify.post("/upload-csv/:type", async (request, reply) => {
-        const { type }: any = request.params;
-        // File size limit 10Mb (in bytes)
-        const file = await request.file();
-        // TODO:
-        // 1. upload file to google drive
-        // 2. store the URL at firestore (collection: csv_reports, document: standard)
-        const result = {
-            code: 201,
-            message: 'UPLOAD_SUCCESSFUL'
-        }
-        reply.code(result?.code!).send({ message: result?.message });
-    });
-
     fastify.post("/upload-report/:docName", async (request, reply) => {
         const { docName }: any = request.params;
         const file = await request.file();
@@ -40,5 +27,9 @@ export async function mainRoute(fastify: FastifyInstance) {
 
     fastify.post("/user/:userId/update-password", async (request, reply) => {
         return await updateUserPassword(request, reply);
+    });
+
+    fastify.post("/upload-csv/:type", async (request, reply) => {
+        return await uploadCSVAndSaveToFirestore(request, reply);
     });
 }
